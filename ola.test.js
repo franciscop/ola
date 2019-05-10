d69366b60ca2efb198cfad0e7cd98d850cf9490e
Francisco Presencia <public@francisco.io>
2019-05-11 03:33:36 +0900

    Initialized repo

---
+++ ola.test.js
@@ -0,0 +1,123 @@
+import Ola from "./ola";
+
+const delay = (time = 300) => new Promise(done => setTimeout(done, time));
+const roundish = (num, dec = 1) => dec * Math.round(num / dec);
+expect.extend({
+  // 10% of approximation by default
+  toApproximate: (a, b, perc = 0.1) => {
+    const pass = Math.abs((a - b) / b) <= perc;
+    return {
+      pass: pass,
+      message: () => `"${a}" ${pass ? "should" : "does not"} approximate "${b}"`
+    };
+  }
+});
+
+describe("Ola", () => {
+  it("is defined", () => {
+    expect(Ola).toBeDefined();
+  });
+});
+
+describe("Ola(0)", () => {
+  it("can be initialized", () => {
+    expect(Ola(0)).toBeDefined();
+  });
+
+  it("can read the value", async () => {
+    const pos = Ola(0);
+    expect(pos.value).toBe(0);
+    expect(pos.get("value")).toBe(0);
+    expect(pos).toEqual({ value: 0 });
+    expect(JSON.stringify(pos)).toBe('{"value":0}');
+  });
+
+  it("can update the value", async () => {
+    const pos = Ola(0);
+    pos.value = 100;
+    await delay();
+
+    expect(pos.value).toBe(100);
+    expect(pos.get("value")).toBe(100);
+    expect(pos).toEqual({ value: 100 });
+    expect(JSON.stringify(pos)).toBe('{"value":100}');
+  });
+
+  it("can use negative numbers", async () => {
+    const pos = Ola(-10);
+    pos.value = -100;
+    await delay();
+
+    expect(pos.value).toBe(-100);
+    expect(pos.get("value")).toBe(-100);
+    expect(pos).toEqual({ value: -100 });
+    expect(JSON.stringify(pos)).toBe('{"value":-100}');
+  });
+
+  it("can update the value with .set()", async () => {
+    const pos = Ola(0);
+    pos.set(100);
+    await delay();
+
+    expect(pos.value).toBe(100);
+    expect(pos.get("value")).toBe(100);
+    expect(pos).toEqual({ value: 100 });
+    expect(JSON.stringify(pos)).toBe('{"value":100}');
+  });
+});
+
+describe("Ola({ x: 0 })", () => {
+  it("can be initialized", () => {
+    expect(Ola({ x: 0 })).toEqual({ x: 0 });
+  });
+
+  it("can read the value", () => {
+    const pos = Ola({ x: 0 });
+    expect(pos.x).toBe(0);
+    expect(pos.get("x")).toBe(0);
+    expect(pos).toEqual({ x: 0 });
+    expect(JSON.stringify(pos)).toBe('{"x":0}');
+  });
+
+  it("can update the value", async () => {
+    const pos = Ola({ x: 0 });
+    pos.x = 100;
+    await delay();
+
+    expect(pos.x).toBe(100);
+    expect(pos.get("x")).toBe(100);
+    expect(pos).toEqual({ x: 100 });
+    expect(JSON.stringify(pos)).toBe('{"x":100}');
+  });
+
+  it("can use negative numbers", async () => {
+    const pos = Ola({ x: -10 });
+    pos.x = -100;
+    await delay();
+
+    expect(pos.x).toBe(-100);
+    expect(pos.get("x")).toBe(-100);
+    expect(pos).toEqual({ x: -100 });
+    expect(JSON.stringify(pos)).toBe('{"x":-100}');
+  });
+
+  it("can update the value with .set()", async () => {
+    const pos = Ola({ x: 0 });
+    pos.set({ x: 100 });
+    await delay();
+
+    expect(pos.x).toBe(100);
+    expect(pos.get("x")).toBe(100);
+    expect(pos).toEqual({ x: 100 });
+    expect(JSON.stringify(pos)).toBe('{"x":100}');
+  });
+});
+
+describe.skip("Ola(..., time, timing)", () => {
+  it("can update with second parameter", async () => {
+    const pos = Ola({ x: 0 });
+    pos.set({ x: 100 }, 600);
+    await delay();
+    expect(pos.x).toApproximate(50);
+  });
+});
---
+++ readme.md
@@ -0,0 +1,135 @@
+# Ola
+
+A smooth interpolation library for Javascript numbers:
+
+<img align="right" width="375" height="180" src="./docs/line.gif">
+
+```js
+// Initialize it to 0
+const temp = Ola(0);
+
+// Set the value randomly (async)
+temp.set(100);
+
+// Log how the value evolves from 0 to 100
+setInterval(() => console.log(temp.value), 10);
+```
+
+It works with multiple values/dimensions:
+
+<img align="right" width="375" height="180" src="./docs/ball.gif">
+
+```js
+// Initialize it to origin
+const pos = Ola({ x: 0, y: 0 });
+
+// Set both values to 100
+pos.set({ x: 100, y: 100 });
+
+// Log how the values evolve
+setInterval(() => console.log(pos.x, pos.y), 10);
+```
+
+> Tip: click on any GIF for the full code used to create it :)
+
+## Getting started
+
+Install it with npm:
+
+```
+npm install ola
+```
+
+Then import it and use it:
+
+```js
+import Ola from "ola";
+const pos = Ola({ x: 0 });
+console.log(pos.x); // 0
+```
+
+If you prefer to use a CDN:
+
+```html
+<script src="https://cdn.jsdelivr.net/npm/ola"></script>
+<script type="text/javascript">
+  const pos = Ola({ x: 0 });
+  console.log(pos.x); // 0
+</script>
+```
+
+## Documentation
+
+There are three distinct operations that can be run: creating an instance, setting it to update and reading it.
+
+### `Ola({ x: 0 }, t)` Create an instance
+
+The first parameter is the initial value. It can be either a single number, or an object of `key:numbers`:
+
+```js
+const tmp = Ola(0); // Alias of `{ value: 0 }`
+const pos = Ola({ x: 0 });
+```
+
+Using a single number is the shortname of using the key `value`. It is offered for convenience, but recommend not mixing both styles in the same project.
+
+The second parameter is how long each update will take in ms.
+
+It works with any kind of float:
+
+```js
+console.log(Ola(100));
+console.log(Ola(-100));
+console.log(Ola(0.001));
+console.log(Ola(1 / 100));
+```
+
+The keys can be any string you prefer, and the instances can be logged as usual:
+
+```js
+console.log(Ola({ abcd: 100 })); // { abcd: 100 }
+JSON.stringify(Ola({ a: 100 })); // '{"a":100}'
+```
+
+> Note: try to keep it safe, so keep your numbers under Number.MAX_VALUE / 10
+
+The time it takes to update can be defined per instance:
+
+```js
+// All `pos.set()` will take 1 full second
+const pos = Ola({ x: 0 }, 1000);
+pos.set({ x: 100 });
+```
+
+> Note: this update time is likely to change in the future
+
+### `pos.x = 10` Update the value
+
+When we update a property **it is not updated instantaneously** (that's the whole point of this library), but instead it's set to update asynchronously:
+
+```js
+const pos = Ola({ x: 0 });
+pos.set({ x: 100 });
+
+// 0 - still hasn't updated
+console.log(pos.x);
+
+// 100 - after 300ms it's fully updated
+setTimeout(() => console.log(pos.x), 1000);
+```
+
+## `pos.x` Read the value
+
+You can read the value at any time:
+
+```js
+const pos = Ola({ x: 0 });
+pos.set({ x: 100 });
+
+setInterval(() => {
+  // It will update every time it's read
+  console.log(pos.x);
+}, 10);
+```
+
+Every time it is read, it'll calculate it based in the current time. So there's _no need to update it_ manually.
